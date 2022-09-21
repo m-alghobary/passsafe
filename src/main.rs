ec5073e6f5d1801211378b86f599c752dd96bd7b
m-alghobary <m.alghobary90@gmail.com>
2022-09-21 21:29:32 +0300

    add clap to parse user args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+
+/// Generate & store your passwords safely
+#[derive(Parser)]
+struct Args {
+
+    /// Name of the app this password is for
+    #[clap(short, long, value_parser)]
+    name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{}", args.name);
 }
