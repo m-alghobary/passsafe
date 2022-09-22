0e88eee0a4703d044a32234aacac02fc45dd0957
m-alghobary <m.alghobary90@gmail.com>
2022-09-22 20:34:42 +0300

    add password re-generate

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,57 @@
 use clap::Parser;
 use rand::distributions::Uniform;
-use rand::{Rng, RngCore, SeedableRng};
 use rand::rngs::StdRng;
+use rand::{Rng, RngCore, SeedableRng};
 
 /// Generate & store your passwords safely
 #[derive(Parser)]
 struct Args {
-
     /// Name of the app this password is for
     #[clap(short, long, value_parser)]
     name: String,
 }
 
 fn main() {
-    // let args = Args::parse();
+    let args = Args::parse();
+    let mut password;
 
-    // println!("{}", args.name);
+    loop {
+        password = gen_password(16);
 
-    println!("{}", gen_password(16));
+        println!("");
+        println!("Pass: {}", password);
+
+        if !re_generate() {
+            break;
+        }
+    }
+
+    println!("");
+    println!("Name: {}", args.name);
+    println!("Pass: {}", password);
 }
 
+fn re_generate() -> bool {
+    println!("");
+    println!("Re-Generate password [Y / N] ?");
+
+    let mut answer = String::new();
+
+    std::io::stdin()
+        .read_line(&mut answer)
+        .expect("Failed to read input!");
+
+    answer.trim().to_lowercase().eq("y")
+}
 
 fn gen_password(len: usize) -> String {
     let letters = get_letter_space();
     let distrib = Uniform::new(0, letters.len());
-    
+
     let mut seed = [0u8; 32];
     rand::thread_rng().fill_bytes(&mut seed);
     let mut rng = StdRng::from_seed(seed);
-    
+
     let mut result = vec![];
     for _ in 0..len {
         result.push(letters[rng.sample(distrib)]);
@@ -37,23 +60,24 @@ fn gen_password(len: usize) -> String {
     result.into_iter().collect()
 }
 
-
 fn get_letter_space() -> Vec<char> {
     let mut letters: Vec<char> = vec![];
 
-    for ch in 'a' as u8 .. 'z' as u8 + 1 {
+    for ch in 'a' as u8..'z' as u8 + 1 {
         letters.push(ch as char);
     }
 
-    for ch in 'A' as u8 .. 'Z' as u8 + 1 {
+    for ch in 'A' as u8..'Z' as u8 + 1 {
         letters.push(ch as char);
     }
 
-    for n in '0' as u8 .. '9' as u8 + 1 {
+    for n in '0' as u8..'9' as u8 + 1 {
         letters.push(n as char);
     }
 
-    let mut special_chars = vec!['!', '#', '$', '%', '&', '*', ']', '[', '(', ')', '{', '}', '+', '-'];
+    let mut special_chars = vec![
+        '!', '#', '$', '%', '&', '*', ']', '[', '(', ')', '{', '}', '+', '-',
+    ];
     letters.append(&mut special_chars);
 
     letters
