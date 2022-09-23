2fcb0258859614c909fb6cb93bc1cd9901752757
m-alghobary <m.alghobary90@gmail.com>
2022-09-23 09:51:22 +0300

    add generator state to control what to use in the password

--- src/passgen/mod.rs
+++ src/passgen/mod.rs
@@ -2,54 +2,70 @@ use rand::distributions::Uniform;
 use rand::rngs::StdRng;
 use rand::{Rng, RngCore, SeedableRng};
 
-
 pub struct PassGenerator {
-    len: usize
+    len: usize,
+    use_upper: bool,
+    use_lower: bool,
+    use_numbers: bool,
+    use_special_chars: bool,
 }
 
 impl PassGenerator {
-    
     pub fn new(len: usize) -> Self {
-        Self { len }
+        Self {
+            len,
+            use_upper: true,
+            use_lower: true,
+            use_numbers: true,
+            use_special_chars: true,
+        }
     }
 
     pub fn gen_password(&self) -> String {
         let letters = self.get_letter_space();
         let distrib = Uniform::new(0, letters.len());
-    
+
         let mut seed = [0u8; 32];
         rand::thread_rng().fill_bytes(&mut seed);
         let mut rng = StdRng::from_seed(seed);
-    
+
         let mut result = vec![];
         for _ in 0..self.len {
             result.push(letters[rng.sample(distrib)]);
         }
-    
+
         result.into_iter().collect()
     }
 
-
     fn get_letter_space(&self) -> Vec<char> {
         let mut letters: Vec<char> = vec![];
-    
-        for ch in 'a' as u8..'z' as u8 + 1 {
-            letters.push(ch as char);
-        }
-    
-        for ch in 'A' as u8..'Z' as u8 + 1 {
-            letters.push(ch as char);
-        }
-    
-        for n in '0' as u8..'9' as u8 + 1 {
-            letters.push(n as char);
-        }
-    
-        let mut special_chars = vec![
-            '!', '#', '$', '%', '&', '*', ']', '[', '(', ')', '{', '}', '+', '-',
-        ];
 
-        letters.append(&mut special_chars);
+        if self.use_lower {
+            for ch in 'a' as u8..'z' as u8 + 1 {
+                letters.push(ch as char);
+            }
+        }
+
+        if self.use_upper {
+            for ch in 'A' as u8..'Z' as u8 + 1 {
+                letters.push(ch as char);
+            }
+        }
+
+        if self.use_numbers {
+            for n in '0' as u8..'9' as u8 + 1 {
+                letters.push(n as char);
+            }
+        }
+
+        if self.use_special_chars {
+            let mut special_chars = vec![
+                '!', '#', '$', '%', '&', '*', ']', '[', '(', ')', '{', '}', '+', '-',
+            ];
+
+            letters.append(&mut special_chars);
+        }
+
         letters
     }
 }
