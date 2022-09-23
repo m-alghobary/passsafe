2836032e9580ef866ae3184689b164be3624a684
m-alghobary <m.alghobary90@gmail.com>
2022-09-23 09:40:44 +0300

    add the full list of args

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,40 @@ mod passgen;
 
 
 /// Generate & store your passwords safely
-#[derive(Parser)]
+#[derive(Parser, Debug, Default)]
 struct Args {
     /// Name of the app this password is for
     #[clap(short, long, value_parser)]
     name: String,
+
+    /// The length of the password
+    #[clap(short, long, value_parser, default_value_t = 8)]
+    len: usize,
+
+    /// No uppercase letters
+    #[clap(long, value_parser)]
+    no_upper: bool,
+    
+    /// No lowercase letters
+    #[clap(long, value_parser)]
+    no_lower: bool,
+
+    /// No numbers
+    #[clap(long, value_parser)]
+    no_numbers: bool,
+
+    /// No special chartactors
+    #[clap(long, value_parser)]
+    no_special_chars: bool,
 }
 
 
 fn main() {
     let args = Args::parse();
-    let mut password;
     let gen = PassGenerator::new(16);
+
+    
+    let mut password;
     loop {
         password = gen.gen_password();
 
